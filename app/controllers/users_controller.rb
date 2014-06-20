b35343e57a5b5724012ce98e91d1616cb72056ff
fimmtiu <dennis@funkplanet.com>
2014-06-20 13:53:10 -0700

    Prevent users from changing other users' data.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :only_myself, only: [:edit, :update]
 
   def index
     @users = User.without_user(current_user)
@@ -10,13 +11,13 @@ class UsersController < ApplicationController
       format.json { render :json =>  @user.to_json(:only => [:id, :status], :methods => [:full_name]) }
     end
   end
-  
+
   def show
-    @user = User.find(params[:id])    
+    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])    
+    @user = User.find(params[:id])
   end
 
   def update
@@ -25,5 +26,13 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  private
+
+  def only_myself
+    unless current_user.id == params[:id].to_i
+      flash[:alert] = "You can't edit other users' information."
+      redirect_to users_path
+    end
+  end
 
 end
