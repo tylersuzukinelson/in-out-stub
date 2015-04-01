d3229d726e0410c3991e46bc7fa3150b42af4c53
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-03-31 18:14:27 -0700

    Fleshed out teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,27 +1,53 @@
 class TeamsController < ApplicationController
 
-  def new
+  before_filter :find_team, only: [:show, :edit, :update, :destroy]
 
+  def new
+    @team = Team.new
   end
 
   def create
-
+    @team = Team.new params[:team][:name]
+    if @team.save
+      redirect_to @team
+    else
+      flash[:alert] = get_errors
+      render :new
+    end
   end
 
   def show
-
   end
 
   def edit
-
   end
 
   def update
-
+    if @team.update name: params[:team][:name]
+      redirect_to @team
+    else
+      redirect_to @team, alert: get_errors
+    end
   end
 
   def destroy
+    if @team.destroy
+      flash[:notice] = "Team deleted."
+      @team = Team.new
+      render :new
+    else
+      redirect_to @team, alert: get_errors
+    end
+  end
 
+  private
+
+  def find_team
+    @team = Team.find params[:id]
+  end
+
+  def get_errors
+    @team.errors.full_messages.join('; ')
   end
 
 end
