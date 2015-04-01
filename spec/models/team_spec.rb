98885fb393b66cb52aaf7a5aa1ae13e9b9caf232
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-04-01 13:32:22 -0700

    Added rspec tests for team model

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Team, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "name validations" do
+
+    context "valid request" do
+
+      it "creates a new team" do
+        team = Team.create name: "Salespeople"
+        expect(Team.last).to eq(team)
+      end
+
+    end
+
+    context "invalid request" do
+
+      context "name does not exist" do
+
+        it "does not create a new team" do
+          team = Team.create name: ""
+          expect(team.errors[:name]).to include("can't be blank")
+        end
+
+      end
+
+      context "name is not unique" do
+
+        it "does not create a new team" do
+          team1 = Team.create name: "Salespeople"
+          team2 = Team.create name: "Salespeople"
+          expect(team2.errors[:name]).to include("has already been taken")
+        end
+
+      end
+
+    end
+
+  end
+
 end
