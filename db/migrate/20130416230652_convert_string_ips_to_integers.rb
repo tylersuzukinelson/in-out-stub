3773dd29a2e8dd88b8b083728b90abc8b8455a46
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-04-01 15:27:17 -0700

    Ensured existing data remains after migration

--- db/migrate/20130416230652_convert_string_ips_to_integers.rb
+++ db/migrate/20130416230652_convert_string_ips_to_integers.rb
@@ -1,12 +1,31 @@
 class ConvertStringIpsToIntegers < ActiveRecord::Migration
 
+  def ip_to_string(address)
+    ip = address
+    return nil unless ip
+    ip += 4_294_967_296 if ip < 0 # Convert from 2's complement
+    "#{(ip & 0xFF000000) >> 24}.#{(ip & 0x00FF0000) >> 16}.#{(ip & 0x0000FF00) >> 8}.#{ip & 0x000000FF}"
+  end
+
   def up
+    User.find_each do |user|
+      user.current_sign_in_ip = user.read_attribute(:current_sign_in_ip) if user.read_attribute(:current_sign_in_ip)
+      user.last_sign_in_ip = user.read_attribute(:last_sign_in_ip) if user.read_attribute(:last_sign_in_ip)
+      user.save!
+    end
     change_column :users, :current_sign_in_ip, :integer
     change_column :users, :last_sign_in_ip,    :integer
   end
 
+  # Before rollback can occur, the IP conversion functions will need to be removed/commented out from the user model
+
   def down
     change_column :users, :current_sign_in_ip, :string
     change_column :users, :last_sign_in_ip,    :string
+    User.find_each do |user|
+      user.current_sign_in_ip = ip_to_string(user.read_attribute(:current_sign_in_ip).to_i) if user.read_attribute(:current_sign_in_ip)
+      user.last_sign_in_ip = ip_to_string(user.read_attribute(:last_sign_in_ip).to_i) if user.read_attribute(:last_sign_in_ip)
+      user.save!
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,21 +20,21 @@ ActiveRecord::Schema.define(:version => 20150401004701) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",                    :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                         :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.integer  "status",                 :default => 0,  :null => false
-    t.string   "first_name",             :default => "", :null => false
-    t.string   "last_name",              :default => "", :null => false
+    t.integer  "current_sign_in_ip",     :limit => 255
+    t.integer  "last_sign_in_ip",        :limit => 255
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
+    t.integer  "status",                                :default => 0,  :null => false
+    t.string   "first_name",                            :default => "", :null => false
+    t.string   "last_name",                             :default => "", :null => false
     t.string   "web_site"
     t.integer  "team_id"
   end
